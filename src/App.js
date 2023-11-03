201c04548667e1ebf13a60f7d41eeb66e3f77073
NakshathraP <119211023+NakshathraP@users.noreply.github.com>
2023-11-03 13:36:12 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,9 +4,6 @@ import TodoComponent from './component/todo';
 const App = () => {
   return (
     <div className="App">
-      <form action="../../post" method="post" className="form">
-        <button type="submit">Connect to React</button>
-      </form>
       <TodoComponent/>
     </div>
   );
